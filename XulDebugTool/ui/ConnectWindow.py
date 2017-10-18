c4ee81706c60d193403e34aff7ea7b6d68de81ff
KenshinTang <28135055@qq.com>
2017-10-18 11:15:04 +0800

    重构命令行执行的相关逻辑. 单独提取出来,方便其他功能使用.

--- XulDebugTool/ui/ConnectWindow.py
+++ XulDebugTool/ui/ConnectWindow.py
@@ -11,14 +11,17 @@ author: Kenshin
 last edited: 2017.10.14
 """
 
-from PyQt5.QtCore import pyqtSlot, Qt, QThread, pyqtSignal
+from PyQt5.QtCore import pyqtSlot, Qt
 from PyQt5.QtWidgets import QPushButton, QLineEdit, QLabel, QTextEdit
 from XulDebugTool.ui.BaseWindow import BaseWindow
+from XulDebugTool.utils.CmdExecutor import CmdExecutor
 
 
 class ConnectWindow(BaseWindow):
     def __init__(self):
         super().__init__()
+        self.cmdExecutor = CmdExecutor()
+        self.cmdExecutor.finishSignal.connect(self.onCmdExectued)
         self.initUI()
         self.show()
 
@@ -68,20 +71,28 @@ class ConnectWindow(BaseWindow):
             self.detail_edit.append('请输出正确的地址or端口.')
             print('请输出正确的地址or端口.')
         else:
-            self.detail_edit.append('adb connect ' + ip)
+            self.currentCmd = 'adb connect ' + ip
+            self.detail_edit.append(self.currentCmd)
             self.connect_button.setEnabled(False)
             self.connect_button.setText('connecting...')
             self.detail_edit.append('connecting...')
-            self.t = ConnectDeviceThread(ip)
-            self.t.finishSignal.connect(self.onConnect)
-            self.t.start()
+            self.cmdExecutor.exec(self.currentCmd)
 
-    def onConnect(self, result):
+    def onCmdExectued(self, result):
         for r in result:
             self.detail_edit.append(r)
             print(r)
-        self.connect_button.setEnabled(True)
-        self.connect_button.setText('connect')
+        if self.currentCmd.startswith('adb connect'):
+            self.connect_button.setEnabled(True)
+            self.connect_button.setText('connect')
+            self.checkDeviceStatus()
+        elif self.currentCmd == 'adb devices':
+            pass
+
+    def checkDeviceStatus(self):
+        self.currentCmd = 'adb devices'
+        self.detail_edit.append(self.currentCmd)
+        self.cmdExecutor.exec(self.currentCmd)
 
     @pyqtSlot()
     def onDetailClick(self):
@@ -93,22 +104,3 @@ class ConnectWindow(BaseWindow):
         else:
             self.setFixedSize(460, 150)
             self.detail_label.setText('↓detail')
-
-
-class ConnectDeviceThread(QThread):
-
-    finishSignal = pyqtSignal(list)
-
-    def __init__(self, ip, parent=None):
-        super(ConnectDeviceThread, self).__init__(parent)
-        self.ip = ip
-
-    def run(self):
-        from subprocess import Popen, PIPE
-        p = Popen('adb connect ' + self.ip, stdout=PIPE, bufsize=1)
-        l = []
-        for line in iter(p.stdout.readline, b''):
-            l.append(line.decode('utf-8'))
-        p.stdout.close()
-        p.wait()
-        self.finishSignal.emit(l)
---
+++ XulDebugTool/utils/CmdExecutor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtCore import QThread, pyqtSignal
+
+
+class CmdExecutor(QThread):
+
+    finishSignal = pyqtSignal(list)
+
+    def __init__(self, parent=None):
+        super(CmdExecutor, self).__init__(parent)
+
+    def exec(self, cmd):
+        self.cmd = cmd
+        self.start()
+
+    def run(self):
+        from subprocess import Popen, PIPE
+        p = Popen(self.cmd, stdout=PIPE, bufsize=1)
+        l = []
+        for line in iter(p.stdout.readline, b''):
+            l.append(line.decode('utf-8'))
+        p.stdout.close()
+        p.wait()
+        self.finishSignal.emit(l)
