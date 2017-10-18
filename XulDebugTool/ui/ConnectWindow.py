d1c830ca3f21ec0f20fc3abeb09eeebb5887848e
KenshinTang <28135055@qq.com>
2017-10-18 10:29:14 +0800

    添加连接设备的详细日志.

--- XulDebugTool/ui/ConnectWindow.py
+++ XulDebugTool/ui/ConnectWindow.py
@@ -12,7 +12,7 @@ last edited: 2017.10.14
 """
 
 from PyQt5.QtCore import pyqtSlot, Qt, QThread, pyqtSignal
-from PyQt5.QtWidgets import QPushButton, QLineEdit, QLabel
+from PyQt5.QtWidgets import QPushButton, QLineEdit, QLabel, QTextEdit
 from XulDebugTool.ui.BaseWindow import BaseWindow
 
 
@@ -45,24 +45,54 @@ class ConnectWindow(BaseWindow):
         self.port_editbox.resize(100, 25)
         self.port_editbox.setText('55550')
 
-        button = QPushButton('connect', self)
-        button.move(180, 90)
-        button.clicked.connect(self.onClick)
+        self.connect_button = QPushButton('connect', self)
+        self.connect_button.move(180, 90)
+        self.connect_button.clicked.connect(self.onConnectClick)
+
+        self.detail_label = QPushButton(self)
+        self.detail_label.setText('↓detail')
+        self.detail_label.move(350, 110)
+        self.detail_label.setFlat(True)
+        self.detail_label.setStyleSheet("QPushButton{background: transparent;}");
+        self.detail_label.clicked.connect(self.onDetailClick)
+
+        self.detail_edit = QTextEdit(self)
+        self.detail_edit.move(25, 150)
+        self.detail_edit.resize(420, 180)
 
     @pyqtSlot()
-    def onClick(self):
+    def onConnectClick(self):
         ip = self.ip_editbox.text()
         port = self.port_editbox.text()
         if ip == '' or port == '':
-            self.statusBar().showMessage('请输出正确的地址or端口.')
+            self.detail_edit.append('请输出正确的地址or端口.')
+            print('请输出正确的地址or端口.')
         else:
+            self.detail_edit.append('adb connect ' + ip)
+            self.connect_button.setEnabled(False)
+            self.connect_button.setText('connecting...')
+            self.detail_edit.append('connecting...')
             self.t = ConnectDeviceThread(ip)
             self.t.finishSignal.connect(self.onConnect)
             self.t.start()
 
     def onConnect(self, result):
         for r in result:
-            self.statusBar().showMessage(r)
+            self.detail_edit.append(r)
+            print(r)
+        self.connect_button.setEnabled(True)
+        self.connect_button.setText('connect')
+
+    @pyqtSlot()
+    def onDetailClick(self):
+        # show log
+        if self.height() == 150:
+            self.setFixedSize(460, 350)
+            self.detail_label.setText('↑detail')
+        # hide log
+        else:
+            self.setFixedSize(460, 150)
+            self.detail_label.setText('↓detail')
 
 
 class ConnectDeviceThread(QThread):
@@ -76,10 +106,8 @@ class ConnectDeviceThread(QThread):
     def run(self):
         from subprocess import Popen, PIPE
         p = Popen('adb connect ' + self.ip, stdout=PIPE, bufsize=1)
-        print('adb connect ' + self.ip)
         l = []
         for line in iter(p.stdout.readline, b''):
-            print(line.decode('utf-8'))
             l.append(line.decode('utf-8'))
         p.stdout.close()
         p.wait()
