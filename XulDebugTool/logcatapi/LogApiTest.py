2c9f1ada4c67d9c788c69ac0b87b2e1d3adb7fef
tieupwithprograming <1145406752@qq.com>
2017-10-30 14:43:10 +0800

    规范化模块，提交日志接口使用示例

---
+++ XulDebugTool/logcatapi/LogApiTest.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+from XulDebugTool.logcatapi.Logcat import STCLogger
+
+STCLogger("cailianyuan").debug("一个debug信息")
+
+STCLogger("cailianyuan").warning("一个warning信息")
+
+STCLogger("cailianyuan").info("一个info信息")
