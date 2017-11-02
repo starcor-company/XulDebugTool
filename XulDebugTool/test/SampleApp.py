736503f9d4518805beffebc4d7df464e6085e21b
KenshinTang <28135055@qq.com>
2017-11-02 19:08:27 +0800

    添加控件测试代码.

---
+++ XulDebugTool/test/SampleApp.py
@@ -0,0 +1,42 @@
+﻿from PyQt5 import QtGui
+import sys
+
+from PyQt5.QtWidgets import *
+
+from XulDebugTool.test.TestClass import TestClass
+from XulDebugTool.ui.widget.PropertyEditor import PropertyEditor
+
+
+class SampleApp(QMainWindow):
+    def __init__(self, parent = None):
+        super(SampleApp, self).__init__(parent)
+        self.setWindowTitle("Property Editor")
+
+        self.widget = QWidget()
+        self.layout = QVBoxLayout()
+        self.widget.setLayout(self.layout)
+
+        self.nodeEdit = PropertyEditor()
+
+        self.button = QPushButton("Print")
+
+        self.test_class = TestClass(self)
+        self.button.clicked.connect(self.get_dict)
+
+        self.nodeEdit.addProperty(self.test_class)
+
+        self.layout.addWidget(self.nodeEdit)
+        self.layout.addWidget(self.button)
+
+        self.setCentralWidget(self.widget)
+        self.show()
+
+    def get_dict(self):
+        print(self.test_class.__dict__)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = SampleApp()
+    window.show()
+    sys.exit(app.exec_())
---
+++ XulDebugTool/test/TestClass.py
@@ -0,0 +1,15 @@
+﻿from PyQt5 import QtCore, QtGui
+
+
+class TestClass(QtCore.QObject):
+
+    def __init__(self, parent = None):
+        super(TestClass, self).__init__(parent)
+        self.name = 'Czesc'
+        self.number = 4
+        #self.color = QtGui.QColor(4)
+        self.radius = 0.00
+        self._id = 5
+        self.test = True
+        #self.lst = [1,2,3,4,5]
+        #self.dict = {'abc' : 1, 'def' : 2}
