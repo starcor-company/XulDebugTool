7b36a499e2c47462549c48701f9838446a1d3600
princebb <RLwx8eH4VVnuJn>
2017-11-15 10:30:46 +0800

    采用字段action识别js下发行为

--- XulDebugTool/webprocess/WebShareObject.py
+++ XulDebugTool/webprocess/WebShareObject.py
@@ -7,10 +7,11 @@ author: Allen
 last edited: 2017.11.7
 
 """
-from PyQt5.QtCore import pyqtProperty
-from PyQt5.QtWidgets import QWidget, QMessageBox
 
-from XulDebugTool.utils.Utils import Utils
+from PyQt5.QtCore import pyqtProperty
+from PyQt5.QtWidgets import QWidget
+
+from XulDebugTool.webprocess.WebDataHandler import WebDataHandler
 
 
 class WebShareObject(QWidget):
@@ -21,18 +22,6 @@ class WebShareObject(QWidget):
         return '100'
 
     def _setStrValue(self, str):
-        list = Utils.findNodeById(str)
-        print('receive %s' % list[0].tag)
-        print('receive %s' % list[0].text)
-        print('receive %s' % list[0].attrib)
-        # for element in list:
-        #     children = element.getchildren()
-        #     if len(children):
-        #         print(element.text + " " + children[0].text)
-        #     else:
-        #         print(element.text)
+        WebDataHandler().readData(str)
 
     strValue = pyqtProperty(str, fget=_getStrValue, fset=_setStrValue)
-
-
-
